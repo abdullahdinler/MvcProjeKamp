109c216f172929bc7c3fd058c9f9d527ffb3c76f
Abdullah Dinler <71527576+abdullahdinler@users.noreply.github.com>
2023-01-18 15:54:33 +0300

    2023/01/18

--- MvcProjeKamp/App_Start/RouteConfig.cs
+++ MvcProjeKamp/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MvcProjeKamp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index2", id = UrlParameter.Optional }
             );
         }
     }
